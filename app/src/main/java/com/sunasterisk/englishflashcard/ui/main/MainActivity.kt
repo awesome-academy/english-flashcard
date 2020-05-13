2bea9bfc33cd198d96b25853dbfa98358054930d
XuanSang98 <nancyvn2000@gmail.com>
2020-05-13 14:59:47 +0700

    Task #23552 : Create UI Splash Screen

--- app/src/main/java/com/sunasterisk/englishflashcard/ui/main/MainActivity.kt
+++ app/src/main/java/com/sunasterisk/englishflashcard/ui/main/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.sunasterisk.englishflashcard.ui.main
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sunasterisk.englishflashcard.R
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        fun getIntent(context: Context) = Intent(context, MainActivity::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/sunasterisk/englishflashcard/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,33 @@
+package com.sunasterisk.englishflashcard.ui.splashscreen
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sunasterisk.englishflashcard.R
+import com.sunasterisk.englishflashcard.ui.main.MainActivity
+
+
+class SplashScreen : AppCompatActivity() {
+
+    companion object {
+        val TIME_LOAD = 3000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val time: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(TIME_LOAD.toLong())
+                } catch (e: Exception) {
+                } finally {
+                    startActivity(MainActivity.getIntent(this@SplashScreen))
+                    finish()
+                }
+            }
+        }
+        time.start()
+    }
+}
