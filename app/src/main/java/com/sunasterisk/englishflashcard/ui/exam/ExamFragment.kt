62844ebacc188ec1e9384381b634975744a7f361
XuanSang98 <nancyvn2000@gmail.com>
2020-05-14 10:49:39 +0700

    Task #23554 : Bottom App Bar

---
+++ app/src/main/java/com/sunasterisk/englishflashcard/ui/exam/ExamFragment.kt
@@ -0,0 +1,8 @@
+package com.sunasterisk.englishflashcard.ui.exam
+
+import com.sunasterisk.englishflashcard.R
+import com.sunasterisk.englishflashcard.ui.BaseFragment
+
+class ExamFragment : BaseFragment() {
+    override val layoutResource get() = R.layout.fragment_exam
+}
---
+++ app/src/main/java/com/sunasterisk/englishflashcard/ui/search/LearnFragment.kt
@@ -0,0 +1,8 @@
+package com.sunasterisk.englishflashcard.ui.search
+
+import com.sunasterisk.englishflashcard.R
+import com.sunasterisk.englishflashcard.ui.BaseFragment
+
+class LearnFragment : BaseFragment() {
+    override val layoutResource get() = R.layout.fragment_learn
+}
